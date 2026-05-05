b93551d39cc53c0742b882453675be15301a3839
Preeti Garg <preeti.garg@preetivrs-36.successivetech.com>
2026-05-05 17:22:23 +0530

    fix: firefox issues, stable tests for assignment 5 & 6

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,9 +26,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
      headless: false,
-  launchOptions: {
-    slowMo: 1000
-  },
+  actionTimeout: 30000,
+  navigationTimeout: 60000,
   video:'on',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
